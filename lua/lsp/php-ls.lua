fe982aac27692a180e4eadd49ae7d9b0c7e0af3c
johnsci911 <johnguitarizta@gmail.com>
2021-04-02 06:28:29 +0800

    gitignore and phpls update

--- lua/lsp/php-ls.lua
+++ lua/lsp/php-ls.lua
@@ -1,9 +1,4 @@
 require'lspconfig'.intelephense.setup {
 	cmd = { DATA_PATH .. "/lspinstall/php/node_modules/.bin/intelephense", "--stdio" },
-	default_config = {
-		init_options = {
-			licenceKey = require('intelephense')
-		}
-	},
     on_attach = require'lsp'.common_on_attach
 }
