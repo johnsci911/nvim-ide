76fcbeef7543f27a5eb85d014c80fe447acd2eb4
johnsci911 <johnguitarizta@gmail.com>
2021-08-03 16:49:29 +0800

    Remove highlight in galaxy short_line

--- lua/config/nvim-galaxyline.lua
+++ lua/config/nvim-galaxyline.lua
@@ -283,16 +283,7 @@ gls.short_line_left[2] = {
 gls.short_line_right[1] = {
   BufferIcon = {
     provider= 'BufferIcon',
-    highlight = { colors.yellow, colors.section_bg },
-    separator = ' ',
-    separator_highlight = { colors.section_bg, colors.bg },
-  }
-}
-
-gls.short_line_right[2] = {
-  shortLastElement = {
-    provider = function() return ' ' end,
-    highlight = { colors.section_bg, colors.section_bg },
+    highlight = { colors.yellow, colors.bg },
   }
 }
 
