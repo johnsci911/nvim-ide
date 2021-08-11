a929a678abaacb1e722b8f0afca0f8a166dc27e2
johnsci911 <johnguitarizta@gmail.com>
2021-08-11 09:38:16 +0800

    Update nvim-tree config

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,4 +1,4 @@
---[[ " 
+--[[ "
 --let g:nvim_tree_auto_ignore_ft = 'startify' "empty by default, don't auto open tree on specific filetypes.
 let g:nvim_tree_hide_dotfiles = 1 "0 by default, this option hides files and folders starting with a dot `.`
 let g:nvim_tree_git_hl = 1 "0 by default, will enable file highlight for git attributes (can be used without the icons).
@@ -15,10 +15,12 @@ let g:nvim_tree_show_icons = {
 vim.g.nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw
 -- vim.g.nvim_tree_hijack_netrw = 0 --"1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
 vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
-vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
+vim.g.nvim_tree_indent_markers = 0 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
-vim.g.nvim_tree_auto_close = 0 -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = 'startify' --empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
+vim.g.nvim_tree_auto_ignore_ft = 'dashboard' --empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
     vim.g.nvim_tree_bindings = {
       -- ["<CR>"] = ":YourVimFunction()<cr>",
@@ -63,4 +65,11 @@ vim.g.nvim_tree_icons = {
     symlink = '',
     git = {unstaged = "", staged = "✓", unmerged = "", renamed = "➜", untracked = ""},
     folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
+}
+
+vim.g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 0,
 }
