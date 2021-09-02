e9a77b3b17355c4bf5cc6d42a05086bce64f7ae6
johnsci911 <johnguitarizta@gmail.com>
2021-09-02 16:18:31 +0800

    Update

--- lua/config/nvim-fzf-lua.lua
+++ lua/config/nvim-fzf-lua.lua
@@ -151,7 +151,7 @@ require'fzf-lua'.setup {
     input_prompt      = 'Grep For❯ ',
     -- cmd               = "rg --vimgrep",
     rg_opts           = "--hidden --column --line-number --no-heading " ..
-                        "--color=always --smart-case -g '!{.git,node_modules,nova,vendor}/*'",
+                        "--color=always --smart-case -g '!{.git,node_modules}/*'",
     git_icons         = true,           -- show git icons?
     file_icons        = true,           -- show file icons?
     color_icons       = true,           -- colorize file|git icons
