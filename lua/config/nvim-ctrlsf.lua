c769feffe248156c8dca5117e6d27de27cf4c5c0
johnsci911 <johnguitarizta@gmail.com>
2021-09-04 10:59:18 +0800

    Clean up

--- lua/config/nvim-ctrlsf.lua
+++
@@ -1,6 +0,0 @@
-vim.g.ctrlsf_search_mode = 'async'
-vim.g.ctrlsf_auto_preview = 1
-vim.g.ctrlsf_case_sensitive = 'yes'
-vim.g.ctrlsf_default_view_mode = 'normal'
-vim.g.ctrlsf_position = 'right'
-vim.g.ctrlsf_winsize = '30%'
