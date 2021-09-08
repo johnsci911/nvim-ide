0bb7ccd3663bf1b3cf68f517c4e897c25d9b62be
johnsci911 <johnguitarizta@gmail.com>
2021-09-08 13:16:58 +0800

    Disabled gitsigns for now

--- lua/config/nvim-telescope.lua
+++ lua/config/nvim-telescope.lua
@@ -3,6 +3,7 @@ local actions = require('telescope.actions')
 ------------------------------
 -- '--color=never',
 require('telescope').load_extension('media_files')
+require('telescope').load_extension('project')
 require('telescope').load_extension('fzf')
 require('telescope').setup {
     defaults = {
