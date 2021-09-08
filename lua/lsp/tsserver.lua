0c47c07c78dfd23582be8e888b1b8baa75fc0072
johnsci911 <johnguitarizta@gmail.com>
2021-09-08 13:01:45 +0800

    Fix indentation

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -9,11 +9,11 @@ require'lspconfig'.tsserver.setup {
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = {
-							spacing = 0,
-							prefix = "",
-							signs = true,
-							underline = true
-						},
+				spacing = 0,
+				prefix = "",
+				signs = true,
+				underline = true
+			},
             signs = true,
             underline = true,
             update_in_insert = true
