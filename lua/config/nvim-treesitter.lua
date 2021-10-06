5406a8ae5e6325bb118735b8df661932db90031b
johnsci911 <johnguitarizta@gmail.com>
2021-10-06 15:04:21 +0800

    Include blade in treesitter config to improve blade highlighting a bit

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -5,7 +5,7 @@ require'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = {
-            "blade",
+            -- "blade",
             "vue"
         }
     },
