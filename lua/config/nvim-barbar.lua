1aad5f4ffc900281b597d1cdaa892c7036d50c0e
johnsci911 <johnguitarizta@gmail.com>
2021-11-05 22:08:22 +0800

    Disable Icon because it has bug

--- lua/config/nvim-barbar.lua
+++ lua/config/nvim-barbar.lua
@@ -30,7 +30,7 @@ vim.g.bufferline = {
   -- Enable/disable icons
   -- if set to 'numbers', will show buffer index in the tabline
   -- if set to 'both', will show buffer index and icons in the tabline
-  icons = true,
+  icons = false,
 
   -- If set, the icon color will follow its corresponding buffer
   -- highlight group. By default, the Buffer*Icon group is linked to the
