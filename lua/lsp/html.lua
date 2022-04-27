bc9e0db5f9c6a8cd9ae802cca5ffef526e9dc4a3
johnsci911 <johnguitarizta@gmail.com>
2022-04-28 03:02:16 +0800

    LSP Update: html

--- lua/lsp/html.lua
+++ lua/lsp/html.lua
@@ -3,12 +3,11 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require'lspconfig'.html.setup {
-    cmd = {"node", DATA_PATH .. "/lspinstall/html/vscode-html/html-language-features/server/dist/node/htmlServerMain.js", "--stdio"},
+    cmd = {DATA_PATH .. "/lsp_servers/html/node_modules/.bin/vscode-html-language-server", "--stdio"},
 	filetypes = {
 		"html",
 		"blade",
 		"php",
 	},
-    on_attach = require'lsp'.common_on_attach,
     capabilities = capabilities
 }
