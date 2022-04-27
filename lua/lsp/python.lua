3560f95b3b4dd07fa840c894f502d4d2e1e4368f
johnsci911 <johnguitarizta@gmail.com>
2022-04-28 03:35:50 +0800

    Update LSP: python

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,6 +1,6 @@
 -- npm i -g pyright
 require'lspconfig'.pyright.setup {
-    cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
+    cmd = {DATA_PATH .. "/lsp_servers/python/node_modules/.bin/pyright-langserver", "--stdio"},
     on_attach = require'lsp'.common_on_attach,
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
