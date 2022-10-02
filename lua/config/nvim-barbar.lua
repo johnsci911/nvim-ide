8f6259b1bde7748419febec45ac6d5daf5687e07
johnsci911 <johnguitarizta@gmail.com>
2022-10-02 09:19:44 +0800

    Moved barbar func to move bar to right of file tree

--- lua/config/nvim-barbar.lua
+++ lua/config/nvim-barbar.lua
@@ -1,6 +1,27 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- Move barbar to right side of File tree
+local nvim_tree_events = require('nvim-tree.events')
+local bufferline_api = require('bufferline.api')
+
+local function get_tree_size()
+  return require'nvim-tree.view'.View.width
+end
+
+nvim_tree_events.subscribe('TreeOpen', function()
+  bufferline_api.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('Resize', function()
+  bufferline_api.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('TreeClose', function()
+  bufferline_api.set_offset(0)
+end)
+--
+
 map('n', '<TAB>', ':BufferNext<CR>', opts)
 map('n', '<S-TAB>', ':BufferPrevious<CR>', opts)
 map('n', '<S-x>', ':BufferClose<CR>', opts)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,26 +25,6 @@ packer.init {
 }
 --
 
--- Move barbar to right side of File tree
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_api = require('bufferline.api')
-
-local function get_tree_size()
-  return require'nvim-tree.view'.View.width
-end
-
-nvim_tree_events.subscribe('TreeOpen', function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('Resize', function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('TreeClose', function()
-  bufferline_api.set_offset(0)
-end)
-
 return packer.startup(function(use)
     -- Packer itself
     use 'wbthomason/packer.nvim'
