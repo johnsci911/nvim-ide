ac69e22af7a83d23596a1c26e8373fe406e980ad
johnsci911 <johnguitarizta@gmail.com>
2022-12-08 19:31:56 +0800

    trouble nvim

--- lua/config/nvim-autopairs.lua
+++ lua/config/nvim-autopairs.lua
@@ -9,26 +9,6 @@ end
 
 _G.MUtils = {}
 
--- TEST
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm = function()
-    if vim.fn.pumvisible() ~= 0 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-            vim.fn["compe#confirm"]()
-            -- return npairs.esc("<c-y>")
-            return npairs.esc("")
-        else
-            vim.defer_fn(function()
-                vim.fn["compe#confirm"]("<cr>")
-            end, 20)
-            return npairs.esc("<c-n>")
-        end
-    else
-        return npairs.check_break_line_char()
-    end
-end
--- TEST
-
 MUtils.completion_confirm = function()
     if vim.fn.pumvisible() ~= 0 then
         if vim.fn.complete_info()["selected"] ~= -1 then
--- lua/config/nvim-trouble.lua
+++ lua/config/nvim-trouble.lua
@@ -1,11 +1,14 @@
-require('trouble').setup {
-	position = "bottom", -- position of the list can be: bottom, top, left, right
+require('trouble').setup
+{
+    position = "bottom", -- position of the list can be: bottom, top, left, right
     height = 10, -- height of the trouble list when position is top or bottom
     width = 50, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
-    mode = "workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
+    mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
     fold_open = "", -- icon used for open folds
     fold_closed = "", -- icon used for closed folds
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
     action_keys = { -- key mappings for actions in the trouble list
         -- map to {} to remove a mapping, for example:
         -- close = {},
@@ -24,7 +27,7 @@ require('trouble').setup {
         close_folds = {"zM", "zm"}, -- close all folds
         open_folds = {"zR", "zr"}, -- open all folds
         toggle_fold = {"zA", "za"}, -- toggle fold of current file
-        previous = "k", -- preview item
+        previous = "k", -- previous item
         next = "j" -- next item
     },
     indent_lines = true, -- add an indent guide below the fold icons
@@ -32,6 +35,7 @@ require('trouble').setup {
     auto_close = false, -- automatically close the list when you have no diagnostics
     auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = false, -- automatically fold a file trouble list at creation
+    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
     signs = {
         -- icons / text used for a diagnostic
         error = "",
@@ -40,5 +44,5 @@ require('trouble').setup {
         information = "",
         other = "﫠"
     },
-    use_lsp_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 }
