be300636a3d6576c56a46df859cb9afb58a6dac5
johnsci911 <johnguitarizta@gmail.com>
2023-02-25 11:37:57 +0800

    Fix intelephense configurations error

--- lua/lsp/php.lua
+++ lua/lsp/php.lua
@@ -1,5 +1,19 @@
 require'lspconfig'.intelephense.setup {
   cmd = { DATA_PATH .. "/mason/packages/intelephense/node_modules/.bin/intelephense", "--stdio" },
-    filetypes = { "php" },
-    root_pattern = {'composer.json', '.git'}
+  filetypes = { "php" },
+  root_pattern = {'composer.json', '.git'},
+  settings = {
+    intelephense = {
+      files = {
+      --   maxSize = 1000000;
+      },
+      environment = {
+        includePaths = {
+          -- "/home/serii/Sites/wordpress",
+          -- "/home/serii/Sites/advanced-custom-fields-pro",
+          -- "/home/serii/Sites/woocommerce"
+        }
+      }
+    }
+  }
 }
