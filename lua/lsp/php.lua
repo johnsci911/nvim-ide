989979e64dbc9d8f3b9a83dad07faf2f3dd41cf8
johnsci911 <johnguitarizta@gmail.com>
2023-02-25 12:02:59 +0800

    Clean

--- lua/lsp/php.lua
+++ lua/lsp/php.lua
@@ -2,18 +2,4 @@ require'lspconfig'.intelephense.setup {
   cmd = { DATA_PATH .. "/mason/packages/intelephense/node_modules/.bin/intelephense", "--stdio" },
   filetypes = { "php" },
   root_pattern = {'composer.json', '.git'},
-  settings = {
-    intelephense = {
-      files = {
-      --   maxSize = 1000000;
-      },
-      environment = {
-        includePaths = {
-          -- "/home/serii/Sites/wordpress",
-          -- "/home/serii/Sites/advanced-custom-fields-pro",
-          -- "/home/serii/Sites/woocommerce"
-        }
-      }
-    }
-  }
 }
--- lua/theme/tokyonight.lua
+++ lua/theme/tokyonight.lua
@@ -1,26 +1,29 @@
--- Tokyonight
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "NvimTree", "qf", "terminal" },
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+})
 
--- The theme comes in two styles, "storm" and a darker variant "night".
-vim.g.tokyonight_style = "storm"
-
--- Comments are italicized by default
-vim.g.tokyonight_italic_comments = true
-
--- Keywords are italicized by default
-vim.g.tokyonight_italic_keywords = true
-
--- Functions are not italicized by default
-vim.g.tokyonight_italic_functions = true
-
--- Enable this to disable setting the background color
-vim.g.tokyonight_transparent = false
-
--- Enabling this option, will hide inactive statuslines and
--- replace them with a thin border instead. Should work with
--- the standard `StatusLine` and `LuaLine`.
-vim.g.tokyonight_hide_inactive_statusline = true
-
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_sidebars = { 'NvimTree' }
-
+-- Call theme twice because IDK why!
 vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[colorscheme tokyonight]]
+
