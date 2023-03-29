5463dea16165839fa1374aa5addecbf35f4667c3
jkcachero <johnguitarizta@gmail.com>
2023-03-29 18:50:25 +0800

    Fix cmp-tabnine config

--- lua/config/nvim-cmp-tabnine.lua
+++ lua/config/nvim-cmp-tabnine.lua
@@ -1,9 +1,5 @@
-local tabnine = require('cmp_tabnine.config')
-
-tabnine:setup({
-    max_lines = 1000;
-    max_num_results = 20;
-    sort = true;
-    run_on_every_keystroke = true;
-    snippet_placeholder = '..';
-})
+require'cmp'.setup {
+  sources = {
+    { name = 'cmp_tabnine' },
+  },
+}
