f8145babfc9ecc7b2a8ca2827009b0d5d3ad467c
jkcachero <johnguitarizta@gmail.com>
2023-04-03 00:10:54 +0800

    Nord disable nord cursorline transparent

--- lua/theme/nord.lua
+++ lua/theme/nord.lua
@@ -2,6 +2,7 @@ vim.g.nord_contrast = false
 vim.g.nord_borders = true
 vim.g.nord_disable_background = false
 vim.g.nord_italic = true
+vim.g.nord_cursorline_transparent = true
 
 -- Load the colorscheme
 require('nord').set()
