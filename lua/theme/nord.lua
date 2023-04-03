9f5b58230c5b8358c26ee0d4bf310246249c71f4
jkcachero <johnguitarizta@gmail.com>
2023-04-03 11:23:52 +0800

    Enable nord border to properly highlight intellisense

--- lua/theme/nord.lua
+++ lua/theme/nord.lua
@@ -1,8 +1,9 @@
-vim.g.nord_contrast = false
-vim.g.nord_borders = true
+vim.g.nord_contrast = true
+vim.g.nord_borders = false
 vim.g.nord_disable_background = false
 vim.g.nord_italic = true
 vim.g.nord_cursorline_transparent = true
+vim.g.nord_enable_sidebar_background = true
 
 -- Load the colorscheme
 require('nord').set()
