6a45976058f117d77ac0db639244c62911f50aad
jkcachero <johnguitarizta@gmail.com>
2023-04-06 16:11:03 +0800

    Opt cleaner indent whitespace highlighting

--- lua/config/nvim-indent-blankline.lua
+++ lua/config/nvim-indent-blankline.lua
@@ -1,10 +1,7 @@
 vim.opt.termguicolors = true
 
 vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1E1E2E gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guibg=#2A2B3C gui=nocombine]]
-
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#696D7F gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#696D7F gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#3B4459 gui=nocombine]]
 
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
@@ -27,13 +24,13 @@ require("indent_blankline").setup {
     enabled = true,
     char_highlight_list = {
         "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
     },
     space_char_highlight_list = {
         "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
     },
+
     space_char_blankline = " ",
     show_current_context = true,
-    colored_indent_levels = false,
+    show_current_context_start = true,
+    colored_indent_levels = true,
 }
