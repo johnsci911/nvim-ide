1ee172cf7ab1f0b88815d45f071a1da545496ce6
jkcachero <johnguitarizta@gmail.com>
2023-05-01 01:06:54 +0800

    Remove deprecated code

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -7,10 +7,6 @@ require'nvim-tree'.setup {
     -- hijack netrw window on startup
     hijack_netrw        = true,
     -- open the tree when running this setup function
-    open_on_setup       = false,
-    -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup  = {},
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab         = false,
     -- hijack the cursor in the tree to put it at the start of the filename
     hijack_cursor       = false,
