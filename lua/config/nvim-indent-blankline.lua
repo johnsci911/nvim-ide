ce3d43a29e72b14d9b2e35230f61c15c0aa80c0f
jkcachero <johnguitarizta@gmail.com>
2023-05-07 01:31:53 +0800

    Disable end of line in blankline plugin

--- lua/config/nvim-indent-blankline.lua
+++ lua/config/nvim-indent-blankline.lua
@@ -18,7 +18,7 @@ vim.g.indentLine_bufNameExclude = {
 
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
     enabled = true,
