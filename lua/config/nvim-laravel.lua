5472ee86fae78c04bea676f52b781024416cdc2c
jkcachero <johnguitarizta@gmail.com>
2023-06-02 22:19:58 +0800

    Fix laravel format

--- lua/config/nvim-laravel.lua
+++ lua/config/nvim-laravel.lua
@@ -1,27 +1,2 @@
 return {
-    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-    keys = {
-        { "<leader>la", ":Laravel artisan<cr>" },
-        { "<leader>lr", ":Laravel routes<cr>" },
-        {
-            "<leader>lt",
-            function()
-                require("laravel.tinker").send_to_tinker()
-            end,
-            mode = "v",
-            desc = "Laravel Application Routes",
-        },
-    },
-    event = { "VeryLazy" },
-    config = function()
-        require("laravel").setup()
-        require("telescope").load_extension "laravel"
-
-        local notify = require("notify")
-        -- this for transparency
-        notify.setup({ background_colour = "#000000" })
-        -- this overwrites the vim notify function
-        vim.notify = notify.notify
-    end,
 }
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -180,5 +180,28 @@ require("lazy").setup({
             "tpope/vim-dotenv",
             "MunifTanjim/nui.nvim",
         },
-    },
+        cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+        keys = {
+            { "<leader>La", ":Laravel artisan<cr>" },
+            { "<leader>Lr", ":Laravel routes<cr>" },
+            {
+                "<leader>Lt",
+                function()
+                    require("laravel.tinker").send_to_tinker()
+                end,
+                mode = "v",
+                desc = "Laravel Application Routes",
+              },
+          },
+        event = { "VeryLazy" },
+        config = function()
+            require("laravel").setup()
+            require("telescope").load_extension "laravel"
+            local notify = require("notify")
+            -- this for transparency
+            notify.setup({ background_colour = "#000000" })
+            -- this overwrites the vim notify function
+            vim.notify = notify.notify
+        end,
+    }
 })
