3b2e589d5f859b7bd603c5fdae52312d68e77e3b
jkcachero <johnguitarizta@gmail.com>
2023-06-02 21:51:06 +0800

    Update laravel config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -178,24 +178,7 @@ require("lazy").setup({
         dependencies = {
             "nvim-telescope/telescope.nvim",
             "tpope/vim-dotenv",
+            "MunifTanjim/nui.nvim",
         },
-        cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-        keys = {
-            { "<leader>La", ":Laravel artisan<cr>" },
-            { "<leader>Lr", ":Laravel routes<cr>" },
-            {
-                "<leader>Lt",
-                function()
-                    require("laravel.tinker").send_to_tinker()
-                end,
-                mode = "v",
-                desc = "Laravel Application Routes",
-            },
-          },
-          event = { "VeryLazy" },
-          config = function()
-            require("laravel").setup()
-            require("telescope").load_extension "laravel"
-        end,
-    }
+    },
 })
