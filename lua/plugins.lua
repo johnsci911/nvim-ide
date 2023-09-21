9eb5e581da9832b340c0bf7aef7a179b961b978f
jkcachero <johnguitarizta@gmail.com>
2023-09-21 21:17:52 +0800

    Remove nvim-notify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -179,7 +179,7 @@ require("lazy").setup({
             -- OPTIONAL:
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
+            -- "rcarriga/nvim-notify",
         }
     },
 
