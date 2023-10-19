40e4d2f13f8322c095d00526a1f25cce3911e8b5
jkcachero <johnguitarizta@gmail.com>
2023-10-19 23:56:40 +0800

    Disable Treesitter highlight for vue because it is buggy

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -2,7 +2,9 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {}
+        disable = {
+            'vue',
+        }
     },
     indent = {
         enable = true,
