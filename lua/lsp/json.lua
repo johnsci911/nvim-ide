3a566614ab3ee76b1c15fbe341bc2234a43503e9
jkcachero <johnguitarizta@gmail.com>
2023-11-13 02:58:11 +0800

    Fix error when saving json files

--- lua/lsp/json.lua
+++ lua/lsp/json.lua
@@ -1,5 +1,10 @@
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 -- npm install -g vscode-json-languageserver
 require'lspconfig'.jsonls.setup {
+  capabilities = capabilities,
   cmd = { "vscode-json-language-server", "--stdio" },
   filetypes = {
     "json",
--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -1,8 +1,17 @@
 require'lspconfig'.tsserver.setup {
   cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx"
+  },
   root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-  settings = {documentFormatting = false},
+  settings = {
+    documentFormatting = false
+  },
   handlers = {
     ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = {
