d271af2369d1ee6b50b03e089cad9f32e176b51e
jkcachero <johnguitarizta@gmail.com>
2024-02-03 18:28:35 +0800

    Highlights now working! 🔥 0.9.2 tree-sitter-blade

---
+++ queries/blade/injections.scm
@@ -0,0 +1,9 @@
+((text) @injection.content
+    (#not-has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language php))
+
+((text) @injection.content
+    (#has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language bash))
