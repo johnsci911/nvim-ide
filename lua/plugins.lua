341dc75335d8b6b58bfafccf23b4870d4cc14204
johnsci911 <johnguitarizta@gmail.com>
2024-02-04 23:02:36 +0800

    always install latest version of telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -96,11 +96,9 @@ require("lazy").setup({
     -- Telescope
     'nvim-lua/popup.nvim',
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                              , branch = '0.1.1',
+        'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-
     {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
@@ -114,6 +112,7 @@ require("lazy").setup({
             'kkharji/sqlite.lua',
         }
     },
+
     "AckslD/nvim-neoclip.lua",
 
     {
