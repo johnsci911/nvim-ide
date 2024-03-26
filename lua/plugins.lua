64c04c1c9223a5b922f15bf8b2ed5d02d82bd23a
johnsci911 <johnguitarizta@gmail.com>
2024-03-27 06:23:42 +0800

    Noice

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,6 +87,23 @@ require("lazy").setup({
     'windwp/nvim-autopairs',
     'posva/vim-vue',
 
+    -- Notification
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
+
     -- Icons
     'kyazdani42/nvim-web-devicons',
     'ryanoasis/vim-devicons',
