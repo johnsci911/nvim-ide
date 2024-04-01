220a88cd2dc541016416fbcad8051aff9a27a3e4
John Karlo <johnkarlo@Johns-iMac-Pro.local>
2024-04-01 12:41:27 +0800

    Gitblame config update

--- lua/config/nvim-gitblame.lua
+++ lua/config/nvim-gitblame.lua
@@ -1,3 +1,7 @@
-vim.g.gitblame_enabled          = 0
-vim.g.gitblame_message_template = ' ==> <summary> • <date> • <author>'
-vim.g.gitblame_date_format      = '%b %d %Y %H:%m'
+require('gitblame').setup {
+    enabled = false,
+    message_template = ' -> <summary> • <date> • <author>',
+    message_when_not_committed = ' -> Not committed',
+    date_format = '%b %d %Y %H:%m',
+    delay = 1000, -- 1 second delay
+}
