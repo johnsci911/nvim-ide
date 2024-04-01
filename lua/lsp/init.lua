ac5395884b5025a9c1343dfb7ddc187403d5b76c
John Karlo <johnkarlo@Johns-iMac-Pro.local>
2024-04-01 14:25:51 +0800

    LSP signs

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -95,6 +95,25 @@ for _, name in pairs(servers) do
     on_attach = on_attach,
     capabilities = capabilities,
     autostart = true,
+    underline = true,
+    update_in_insert = false,
+    virtual_text = {
+      spacing = 2,
+      source = "if_many",
+      prefix = "●",
+      -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+      -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+      -- prefix = "icons",
+    },
+    severity_sort = true,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = " ",
+        [vim.diagnostic.severity.WARN] = " ",
+        [vim.diagnostic.severity.HINT] = " ",
+        [vim.diagnostic.severity.INFO] = " ",
+      },
+    },
     settings = {
       Lua = {
         diagnostics = {
