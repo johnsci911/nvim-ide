da4d46b8662fb4f3b395f567f1544561d4b53d83
John Karlo <johnkarlo@Johns-iMac-Pro.local>
2024-04-04 19:49:55 +0800

    yaml for github workflows

--- lua/lsp/yaml.lua
+++ lua/lsp/yaml.lua
@@ -1,4 +1,11 @@
 -- npm install -g yaml-language-server
 require 'lspconfig'.yamlls.setup {
   cmd = { "yaml-language-server", "--stdio" },
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+      },
+    },
+  }
 }
