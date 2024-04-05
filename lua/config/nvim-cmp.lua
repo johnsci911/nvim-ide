fdeb8a3b4a67bfef26bdace5304335aa4ccd83f0
John Karlo <johnkarlo@Johns-iMac-Pro.local>
2024-04-05 22:33:38 +0800

    nvim-cmp and tabnine config update

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -30,7 +30,7 @@ cmp.setup {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-space>'] = cmp.mapping.complete(),
         ['<C-c>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
+        ['<Tab>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         })
--- lua/config/nvim_tabnine.lua
+++ lua/config/nvim_tabnine.lua
@@ -1,7 +1,7 @@
 require('tabnine').setup({
   disable_auto_comment=true,
-  accept_keymap="<Tab>",
-  dismiss_keymap = "<C-]>",
+  -- accept_keymap="<Tab>",
+  -- dismiss_keymap = "<C-]>",
   debounce_ms = 800,
   suggestion_color = {gui = "#808080", cterm = 244},
   exclude_filetypes = {"TelescopePrompt", "NvimTree"},
