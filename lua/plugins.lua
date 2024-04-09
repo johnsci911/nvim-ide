75030cf23c6b720d0e42c8f293720089fda506d7
jkcachero <johnguitarizta@gmail.com>
2024-04-09 17:31:11 +0800

    Lock on v7

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -191,8 +191,8 @@ require("lazy").setup({
     {
         'nvim-neorg/neorg',
         dependencies = { 'luarocks.nvim' },
-        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "*", -- Pin Neorg to the latest stable release
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "v7.0.0",
         config = true,
     },
     {
