eb8dd3e706d614ffd8415e22c3c4f7f4e9de668c
jkcachero <johnguitarizta@gmail.com>
2024-04-09 17:30:09 +0800

    Remove rainbow option in treesitter

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -71,15 +71,6 @@ require 'nvim-treesitter.configs'.setup {
     refactor = {
         highlight_definitions = { enable = true }
     },
-    rainbow = {
-        enable = true,
-        -- list of languages you want to disable the plugin for
-        disable = { 'jsx', 'cpp' },
-        -- Which query to use for finding delimiters
-        query = 'rainbow-parens',
-        -- Highlight the entire buffer all at once
-        strategy = require('ts-rainbow').strategy.global,
-    },
     incremental_selection = {
         enable = true,
         keymaps = {
