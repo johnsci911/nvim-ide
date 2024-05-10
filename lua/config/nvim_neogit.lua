7914d8694dc6a8113c34764241c50b73dca82b23
jkcachero <johnguitarizta@gmail.com>
2024-05-10 19:07:10 +0800

    Neogit change branch to master

--- lua/config/nvim_neogit.lua
+++ lua/config/nvim_neogit.lua
@@ -19,7 +19,7 @@ neogit.setup {
   },
   -- "ascii"   is the graph the git CLI generates
   -- "unicode" is the graph like https://github.com/rbong/vim-flog
-  graph_style = "ascii",
+  graph_style = "ascii", 
   -- Used to generate URL's for branch popup action "pull request".
   git_services = {
     ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
@@ -78,7 +78,7 @@ neogit.setup {
   },
   commit_view = {
     kind = "vsplit",
-    verify_commit = os.execute("which gpg") == 0, -- Can be set to true or false, otherwise we try to find the binary
+    verify_commit = vim.fn.executable("gpg") == 1, -- Can be set to true or false, otherwise we try to find the binary
   },
   log_view = {
     kind = "tab",
@@ -111,7 +111,7 @@ neogit.setup {
   integrations = {
     -- If enabled, use telescope for menu selection rather than vim.ui.select.
     -- Allows multi-select and some things that vim.ui.select doesn't.
-    telescope = true,
+    telescope = nil,
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
     -- The diffview integration enables the diff popup.
     --
