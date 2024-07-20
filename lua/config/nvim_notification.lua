3ed93a4e25d9894d903804e6774d9f51eef540e2
jkcachero <johnguitarizta@gmail.com>
2024-07-21 03:16:06 +0800

    Buffer Next/Prev fixed using barbar's builtin keymaps Formatting

--- lua/config/nvim_notification.lua
+++ lua/config/nvim_notification.lua
@@ -1,103 +1,117 @@
 require('notify').setup {
-    background_colour = "#000000",
-    render = "default",
-    stages = "fade_in_slide_out",
-    time_formats = {
-        notification = " %a %b %d, %Y -  %H:%M",
-        notification_history = " %a %b %d, %Y -  %H:%M"
-    },
-    max_height = 3,
+  background_colour = "#000000",
+  render = "default",
+  stages = "fade_in_slide_out",
+  time_formats = {
+    notification = " %a %b %d, %Y -  %H:%M",
+    notification_history = " %a %b %d, %Y -  %H:%M"
+  },
+  max_height = 3,
 }
 
 require('noice').setup {
-    lsp = {
-        progress = {
-            enabled = true,
-            -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
-            -- See the section on formatting for more details on how to customize.
-            --- @type NoiceFormat|string
-            format = "lsp_progress",
-            --- @type NoiceFormat|string
-            format_done = "lsp_progress_done",
-            throttle = 1000 / 30, -- frequency to update lsp progress message
-            view = "mini",
-        },
-        override = {
-            -- override the default lsp markdown formatter with Noice
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-            -- override the lsp markdown formatter with Noice
-            ["vim.lsp.util.stylize_markdown"] = false,
-            -- override cmp documentation with Noice (needs the other options to work)
-            ["cmp.entry.get_documentation"] = false,
-        },
-        hover = {
-            enabled = true,
-            silent = true, -- set to true to not show a message if hover is not available
-            view = nil,     -- when nil, use defaults from documentation
-            ---@type NoiceViewOptions
-            opts = {},      -- merged with defaults from documentation
-        },
-        signature = {
-            enabled = true,
-            auto_open = {
-                enabled = true,
-                trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-                luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-                throttle = 50,  -- Debounce lsp signature help request by 50ms
-            },
-            view = nil,         -- when nil, use defaults from documentation
-            ---@type NoiceViewOptions
-            opts = {},          -- merged with defaults from documentation
-        },
-        message = {
-            -- Messages shown by lsp servers
-            enabled = true,
-            view = "notify",
-            opts = {},
-        },
-        -- defaults for hover and signature help
-        documentation = {
-            view = "hover",
-            ---@type NoiceViewOptions
-            opts = {
-                lang = "markdown",
-                replace = true,
-                render = "plain",
-                format = { "{message}" },
-                win_options = { concealcursor = "n", conceallevel = 3 },
-            },
-        },
+  lsp = {
+    progress = {
+      enabled = true,
+      -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+      -- See the section on formatting for more details on how to customize.
+      --- @type NoiceFormat|string
+      format = "lsp_progress",
+      --- @type NoiceFormat|string
+      format_done = "lsp_progress_done",
+      throttle = 1000 / 30,       -- frequency to update lsp progress message
+      view = "notify",
     },
-    presets = {
-        bottom_search = true,         -- use a classic bottom cmdline for search
-        command_palette = true,       -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
+    override = {
+      -- override the default lsp markdown formatter with Noice
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      -- override the lsp markdown formatter with Noice
+      ["vim.lsp.util.stylize_markdown"] = false,
+      -- override cmp documentation with Noice (needs the other options to work)
+      ["cmp.entry.get_documentation"] = false,
     },
-    routes = {
-        {
-            filter = {
-                event = "msg_showmode",
-                any = {
-                    { find = "%d+L, %d+B" },
-                    { find = "; after #%d+" },
-                    { find = "; before #%d+" },
-                },
-            },
-            view = "notify",
+    hover = {
+      enabled = true,
+      silent = true,       -- set to true to not show a message if hover is not available
+      view = nil,          -- when nil, use defaults from documentation
+      ---@type NoiceViewOptions
+      opts = {},           -- merged with defaults from documentation
+    },
+    signature = {
+      enabled = true,
+      auto_open = {
+        enabled = true,
+        trigger = true,         -- Automatically show signature help when typing a trigger character from the LSP
+        luasnip = true,         -- Will open signature help when jumping to Luasnip insert nodes
+        throttle = 50,          -- Debounce lsp signature help request by 50ms
+      },
+      view = nil,               -- when nil, use defaults from documentation
+      ---@type NoiceViewOptions
+      opts = {},                -- merged with defaults from documentation
+    },
+    message = {
+      -- Messages shown by lsp servers
+      enabled = true,
+      view = "notify",
+      opts = {},
+    },
+    -- defaults for hover and signature help
+    documentation = {
+      view = "hover",
+      ---@type NoiceViewOptions
+      opts = {
+        lang = "markdown",
+        replace = true,
+        render = "plain",
+        format = { "{message}" },
+        win_options = { concealcursor = "n", conceallevel = 3 },
+      },
+    },
+  },
+  presets = {
+    bottom_search         = true,     -- use a classic bottom cmdline for search
+    command_palette       = true,     -- position the cmdline and popupmenu together
+    long_message_to_split = true,     -- long messages will be sent to a split
+    inc_rename            = true,     -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border        = true,     -- add a border to hover docs and signature help
+  },
+  routes = {
+    {
+      filter = {
+        event = "msg_showmode",
+        any = {
+          { find = "%d+L, %d+B" },
+          { find = "; after #%d+" },
+          { find = "; before #%d+" },
         },
+      },
+      view = "notify",
     },
-    views = {
-        cmdline_popup = {
-            border = {
-                style = "none",
-                padding = { 2, 3 },
-            },
-            filter_options = {},
-            win_options = {
-                winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-            },
-        },
+  },
+  markdown = {
+    hover = {
+      ["|(%S-)|"] = vim.cmd.help,                             -- vim help links
+      ["%[.-%]%((%S-)%)"] = require("noice.util").open,       -- markdown links
     },
+    highlights = {
+      ["|%S-|"] = "@text.reference",
+      ["@%S+"] = "@parameter",
+      ["^%s*(Parameters:)"] = "@text.title",
+      ["^%s*(Return:)"] = "@text.title",
+      ["^%s*(See also:)"] = "@text.title",
+      ["{%S-}"] = "@parameter",
+    },
+  },
+  views = {
+    cmdline_popup = {
+      border = {
+        style = "none",
+        padding = { 2, 3 },
+      },
+      filter_options = {},
+      win_options = {
+        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      },
+    },
+  },
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,259 +6,259 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",     -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    -- Lazyvim
-    'folke/lazy.nvim',
-    {
-        'stevearc/aerial.nvim',
-        opts = {},
-        -- Optional dependencies
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
+  -- Lazyvim
+  'folke/lazy.nvim',
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
     },
+  },
 
-    -- LSP
-    {
-        "mhartington/formatter.nvim",
-        event = "VeryLazy",
-        opts = function()
-            return require "config.nvim-formatter"
-        end
+  -- LSP
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "config.nvim-formatter"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        'eslint-lsp',
+        'prettier',
+        'typescript-language-server',
+      }
+    }
+  },
+  {
+    'folke/trouble.nvim',
+    branch = "main",
+  },
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+
+  'onsails/lspkind-nvim',
+  'kosayoda/nvim-lightbulb',
+
+  -- Indentations
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
+  "echasnovski/mini.indentscope",
+
+  -- Debug Lint
+  'mfussenegger/nvim-dap',
+  'mfussenegger/nvim-lint',
+
+  -- Autocomplete
+  'mattn/emmet-vim',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'roobert/tailwindcss-colorizer-cmp.nvim',
+  'hrsh7th/vim-vsnip',
+  'hrsh7th/cmp-vsnip',
+  {
+    'tzachar/cmp-tabnine',
+    build = './install.sh',
+    dependencies = 'hrsh7th/nvim-cmp',
+  },
+  {
+    'codota/tabnine-nvim',
+    build = "./dl_binaries.sh"
+  },
+
+  -- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
+  'windwp/nvim-ts-autotag',
+  'nvim-treesitter/nvim-treesitter-context',
+  'HiPhish/nvim-ts-rainbow2',
+  'nvim-treesitter/playground',
+  'windwp/nvim-autopairs',
+
+  -- Notification
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
     },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                'eslint-lsp',
-                'prettier',
-                'typescript-language-server',
-            }
-        }
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
+
+  -- Icons
+  'kyazdani42/nvim-web-devicons',
+  'ryanoasis/vim-devicons',
+
+  -- Status Line and Bufferline TODO: find tabbar plugin for 0.10.^
+  'romgrk/barbar.nvim',
+
+  -- Keymappings
+  'folke/which-key.nvim',
+  'echasnovski/mini.icons',
+
+  -- Colors
+  'NvChad/nvim-colorizer.lua',
+
+  -- Git
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "sindrets/diffview.nvim",       -- optional - Diff integration
     },
-    {
-        'folke/trouble.nvim',
-        branch = "main",
+    config = true,
+  },
+  'f-person/git-blame.nvim',
+  'lewis6991/gitsigns.nvim',
+
+  -- Telescope
+  'nvim-lua/popup.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      "Snikimonkd/telescope-git-conflicts.nvim",
+    }
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build =
+    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  },
+
+  -- Code preview
+  {
+    'michaelrommel/nvim-silicon',
+    lazy = true,
+    cmd = "Silicon",
+  },
+
+  'nvim-telescope/telescope-media-files.nvim',
+  'nvim-telescope/telescope-project.nvim',
+  {
+    'dhruvmanila/telescope-bookmarks.nvim',
+    dependencies = {
+      'kkharji/sqlite.lua',
+    }
+  },
+
+  "AckslD/nvim-neoclip.lua",
+
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    -- stylua: ignore
+    keys = {
+      { "<leader>Ss", function() require("persistence").load() end,                desc = "Restore Session" },
+      { "<leader>Sl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>Sd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
     },
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
+  },
 
-    'onsails/lspkind-nvim',
-    'kosayoda/nvim-lightbulb',
+  -- Org Mode
+  {
+    'nvim-neorg/neorg',
+    dependencies = { 'luarocks.nvim' },
+    lazy = false,     -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "v7.0.0",
+    config = true,
+  },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
 
-    -- Indentations
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-    },
-    "echasnovski/mini.indentscope",
+  'ghassan0/telescope-glyph.nvim',
+  'ibhagwan/fzf-lua',
+  'dyng/ctrlsf.vim',
 
-    -- Debug Lint
-    'mfussenegger/nvim-dap',
-    'mfussenegger/nvim-lint',
+  -- Themes
+  'folke/tokyonight.nvim',
+  'marko-cerovac/material.nvim',
+  {
+    "catppuccin/nvim",
+    name = "catppuccin"
+  },
 
-    -- Autocomplete
-    'mattn/emmet-vim',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'roobert/tailwindcss-colorizer-cmp.nvim',
-    'hrsh7th/vim-vsnip',
-    'hrsh7th/cmp-vsnip',
-    {
-        'tzachar/cmp-tabnine',
-        build = './install.sh',
-        dependencies = 'hrsh7th/nvim-cmp',
-    },
-    {
-        'codota/tabnine-nvim',
-        build = "./dl_binaries.sh"
-    },
+  -- Navigation
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+  },
+  'nacro90/numb.nvim',
+  'kyazdani42/nvim-tree.lua',
 
-    -- Treesitter
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
-    },
-    'windwp/nvim-ts-autotag',
-    'nvim-treesitter/nvim-treesitter-context',
-    'HiPhish/nvim-ts-rainbow2',
-    'nvim-treesitter/playground',
-    'windwp/nvim-autopairs',
+  -- Better Align
+  'junegunn/vim-easy-align',
 
-    -- Notification
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- add any options here
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        }
-    },
+  -- Start Screen
+  -- Dashboard not working properly
 
-    -- Icons
-    'kyazdani42/nvim-web-devicons',
-    'ryanoasis/vim-devicons',
+  -- Close buffer
+  'moll/vim-bbye',
 
-    -- Status Line and Bufferline TODO: find tabbar plugin for 0.10.^
-    'romgrk/barbar.nvim',
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  -- Floating Terminal
+  'voldikss/vim-floaterm',
 
-    -- Keymappings
-    'folke/which-key.nvim',
-    'echasnovski/mini.icons',
+  -- Comment
+  'terrortylor/nvim-comment',
 
-    -- Colors
-    'NvChad/nvim-colorizer.lua',
+  -- Project Rooter
+  "airblade/vim-rooter",
 
-    -- Git
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "sindrets/diffview.nvim", -- optional - Diff integration
-        },
-        config = true,
-    },
-    'f-person/git-blame.nvim',
-    'lewis6991/gitsigns.nvim',
+  -- Multi cursor support
+  'mg979/vim-visual-multi',
 
-    -- Telescope
-    'nvim-lua/popup.nvim',
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            "Snikimonkd/telescope-git-conflicts.nvim",
-        }
-    },
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build =
-        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    },
+  -- Strip WhiteSpace
+  "ntpeters/vim-better-whitespace",
 
-    -- Code preview
-    {
-        'michaelrommel/nvim-silicon',
-        lazy = true,
-        cmd = "Silicon",
-    },
+  -- EWW language support
+  "elkowar/yuck.vim",
 
-    'nvim-telescope/telescope-media-files.nvim',
-    'nvim-telescope/telescope-project.nvim',
-    {
-        'dhruvmanila/telescope-bookmarks.nvim',
-        dependencies = {
-            'kkharji/sqlite.lua',
-        }
-    },
+  -- Dockerfile syntax highlighting
+  "ekalinin/Dockerfile.vim",
 
-    "AckslD/nvim-neoclip.lua",
+  -- Galaxyline
+  'glepnir/galaxyline.nvim',
 
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
-        -- stylua: ignore
-        keys = {
-            { "<leader>Ss", function() require("persistence").load() end,                desc = "Restore Session" },
-            { "<leader>Sl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-            { "<leader>Sd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
-        },
-    },
-
-    -- Org Mode
-    {
-        'nvim-neorg/neorg',
-        dependencies = { 'luarocks.nvim' },
-        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "v7.0.0",
-        config = true,
-    },
-    {
-        "vhyrro/luarocks.nvim",
-        priority = 1000,
-        config = true,
-    },
-
-    'ghassan0/telescope-glyph.nvim',
-    'ibhagwan/fzf-lua',
-    'dyng/ctrlsf.vim',
-
-    -- Themes
-    'folke/tokyonight.nvim',
-    'marko-cerovac/material.nvim',
-    {
-        "catppuccin/nvim",
-        name = "catppuccin"
-    },
-
-    -- Navigation
-    {
-      "folke/flash.nvim",
-      event = "VeryLazy",
-    },
-    'nacro90/numb.nvim',
-    'kyazdani42/nvim-tree.lua',
-
-    -- Better Align
-    'junegunn/vim-easy-align',
-
-    -- Start Screen
-    -- Dashboard not working properly
-
-    -- Close buffer
-    'moll/vim-bbye',
-
-    -- Markdown Preview
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    -- Floating Terminal
-    'voldikss/vim-floaterm',
-
-    -- Comment
-    'terrortylor/nvim-comment',
-
-    -- Project Rooter
-    "airblade/vim-rooter",
-
-    -- Multi cursor support
-    'mg979/vim-visual-multi',
-
-    -- Strip WhiteSpace
-    "ntpeters/vim-better-whitespace",
-
-    -- EWW language support
-    "elkowar/yuck.vim",
-
-    -- Dockerfile syntax highlighting
-    "ekalinin/Dockerfile.vim",
-
-    -- Galaxyline
-    'glepnir/galaxyline.nvim',
-
-    -- Auto tab width
-    'tpope/vim-sleuth',
+  -- Auto tab width
+  'tpope/vim-sleuth',
 })
