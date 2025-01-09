c5c2802b85d6161ce4003db6a9bac47ab7bcaa1a
jkcachero <johnguitarizta@gmail.com>
2025-01-10 03:29:56 +0800

    Experimental - GitGraph

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -119,10 +119,36 @@ require("lazy").setup({
     },
     config = true,
   },
-  -- {
-  --   'isakbm/gitgraph.nvim',
-  --   dependencies = { 'sindrets/diffview.nvim' },
-  -- },
+  {
+    'isakbm/gitgraph.nvim',
+    opts = {
+      symbols = {
+        merge_commit = 'M',
+        commit = '*',
+      },
+      format = {
+        timestamp = '%H:%M:%S %d-%m-%Y',
+        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+      },
+      hooks = {
+        on_select_commit = function(commit)
+          print('selected commit:', commit.hash)
+        end,
+        on_select_range_commit = function(from, to)
+          print('selected range:', from.hash, to.hash)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>gl",
+        function()
+          require('gitgraph').draw({}, { all = true, max_count = 5000 })
+        end,
+        desc = "GitGraph - Draw",
+      },
+    },
+  },
   'f-person/git-blame.nvim',
   'lewis6991/gitsigns.nvim',
 
