da9be61a4d8fc153a42bbbab8bb3d977b52515d8
jkcachero <johnguitarizta@gmail.com>
2025-05-06 10:11:10 +0800

    Add keybindings for snacks.nvim Dashboard

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,6 +61,16 @@ require("lazy").setup({
    / |/ / | / /  _/  |/  /___/ _ \/ _ \/ __/
 /    /| |/ // // /|_/ /___/ ___/ // / _/
  /_/|_/ |___/___/_/  /_/   /_/  /____/___/  ]],
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
         },
         sections = {
           {
