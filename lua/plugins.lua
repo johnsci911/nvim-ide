9372428e7c97031684f0901e9d9b025348a11707
jkcachero <johnguitarizta@gmail.com>
2025-05-22 20:58:17 +0800

    Typo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -135,171 +135,108 @@ require("lazy").setup({
   },
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
     version = "0.0.15",
+    event = "VeryLazy",
     lazy = false,
 
+    -- OpenAI
+    -- opts = {
+    --   provider = "openai",
+    --   auto_suggestions_provider = "openai",
+    --   openai = {
+    --     endpoint = "https://api.openai.com/v1",
+    --     model = "gpt-4o-mini",
+    --     timeout = 30000, -- timeout in milliseconds
+    --     temperature = 0,
+    --     max_tokens = 4096,
+    --   },
+    -- },
+
     -- Ollama
-    {
-      "yetone/avante.nvim",
-      version = "0.0.15",
-      event = "VeryLazy",
-      lazy = false,
-
-      -- OpenAI
-      -- opts = {
-      --   provider = "openai",
-      --   auto_suggestions_provider = "openai",
-      --   openai = {
-      --     endpoint = "https://api.openai.com/v1",
-      --     model = "gpt-4o-mini",
-      --     timeout = 30000, -- timeout in milliseconds
-      --     temperature = 0,
-      --     max_tokens = 4096,
-      --   },
-      -- },
-
-      -- Ollama
-      opts = {
-        -- provider = "openai",
-        provider = "ollama",
-        use_absolute_path = true,
-        openai = {
-          endpoint = "https://api.openai.com/v1",
-          model = "gpt-4o-mini",
-          timeout = 30000, -- timeout in milliseconds
-          temperature = 0,
-          max_tokens = 4096,
-        },
-        vendors = {
-          ollama = {
-            __inherited_from = "openai",
-            api_key_name = "",
-            endpoint = "http://127.0.0.1:11434/v1",
-            -- model = "incept5/llama3.1-claude:latest", -- Llama3.1 with Anthropic's Claude Sonnet 3.5 prompt
-            -- model = "qwen2.5-coder:7b", -- Qwen 2.5 Coder 7B
-            model = "GandalfBaum/llama3.2-claude3.7:latest",
-          },
-        },
-        -- dual_boost = {
-        --   enabled = false,
-        --   first_provider = "ollama",
-        --   second_provider = "openai",
-        --   prompt =
-        --   "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
-        --   timeout = 60000, -- Timeout in milliseconds
-        -- },
-        behaviour = {
-          auto_suggestions = false, -- I have tabnine to handle this
-          auto_set_highlight_group = true,
-          auto_set_keymaps = true,
-          auto_apply_diff_after_generation = false,
-          support_paste_from_clipboard = true,
-        },
-        mappings = {
-          --- @class AvanteConflictMappings
-          diff = {
-            ours = 'co',
-            theirs = 'ct',
-            all_theirs = 'ca',
-            both = 'cb',
-            cursor = 'cc',
-            next = ']x',
-            prev = '[x',
-          },
-          suggestion = {
-            accept = '<M-l>',
-            next = '<M-]>',
-            prev = '<M-[>',
-            dismiss = '<C-]>',
-          },
-          jump = {
-            next = ']]',
-            prev = '[[',
-          },
-          submit = {
-            normal = '<CR>',
-            insert = '<C-s>',
-          },
-        },
-        hints = { enabled = true },
-        windows = {
-          ---@type "right" | "left" | "top" | "bottom"
-          position = 'right', -- the position of the sidebar
-          wrap = true,        -- similar to vim.o.wrap
-          width = 40,         -- default % based on available width
-          sidebar_header = {
-            align = 'center', -- left, center, right for title
-            rounded = true,
-          },
-        },
-        highlights = {
-          ---@type AvanteConflictHighlights
-          diff = {
-            current = 'DiffText',
-            incoming = 'DiffAdd',
-          },
-        },
-        --- @class AvanteConflictUserConfig
-        diff = {
-          autojump = true,
-          ---@type string | fun(): any
-          list_opener = 'copen',
+    opts = {
+      -- provider = "openai",
+      provider = "ollama",
+      use_absolute_path = true,
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o-mini",
+        timeout = 30000, -- timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096,
+      },
+      vendors = {
+        ollama = {
+          __inherited_from = "openai",
+          api_key_name = "",
+          endpoint = "http://127.0.0.1:11434/v1",
+          -- model = "incept5/llama3.1-claude:latest", -- Llama3.1 with Anthropic's Claude Sonnet 3.5 prompt
+          model = "qwen2.5-coder:7b", -- Qwen 2.5 Coder 7B
         },
       },
-
-      -- Deep Seek model integration
-      -- opts = {
-      --   provider = "openai",
-      --   auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-      --   openai = {
-      --     endpoint = "https://api.deepseek.com/v1",
-      --     model = "deepseek-chat",
-      --     timeout = 30000, -- Timeout in milliseconds
-      --     temperature = 0,
-      --     max_tokens = 4096,
-      --   },
+      -- dual_boost = {
+      --   enabled = false,
+      --   first_provider = "ollama",
+      --   second_provider = "openai",
+      --   prompt =
+      --   "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      --   timeout = 60000, -- Timeout in milliseconds
       -- },
-      --
-      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-      build = "make",
-      -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-      dependencies = {
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",        -- for providers='copilot'
-        {
-          -- support for image pasting
-          "HakonHarnes/img-clip.nvim",
-          event = "VeryLazy",
-          opts = {
-            -- recommended settings
-            default = {
-              embed_image_as_base64 = false,
-              prompt_for_file_name = false,
-              drag_and_drop = {
-                insert_mode = true,
-              },
-              -- required for Windows users
-              use_absolute_path = true,
-            },
-          },
+      behaviour = {
+        auto_suggestions = false, -- I have tabnine to handle this
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+      },
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = 'co',
+          theirs = 'ct',
+          all_theirs = 'ca',
+          both = 'cb',
+          cursor = 'cc',
+          next = ']x',
+          prev = '[x',
         },
-        {
-          -- Make sure to set this up properly if you have lazy=true
-          'MeanderingProgrammer/render-markdown.nvim',
-          opts = {
-            file_types = { "markdown", "Avante" },
-          },
-          ft = { "markdown", "Avante" },
+        suggestion = {
+          accept = '<M-l>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
         },
+        jump = {
+          next = ']]',
+          prev = '[[',
+        },
+        submit = {
+          normal = '<CR>',
+          insert = '<C-s>',
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        ---@type "right" | "left" | "top" | "bottom"
+        position = 'right', -- the position of the sidebar
+        wrap = true,        -- similar to vim.o.wrap
+        width = 40,         -- default % based on available width
+        sidebar_header = {
+          align = 'center', -- left, center, right for title
+          rounded = true,
+        },
+      },
+      highlights = {
+        ---@type AvanteConflictHighlights
+        diff = {
+          current = 'DiffText',
+          incoming = 'DiffAdd',
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        ---@type string | fun(): any
+        list_opener = 'copen',
       },
     },
 
@@ -357,7 +294,6 @@ require("lazy").setup({
       },
     },
   },
-
   -- Treesitter
   {
     'nvim-treesitter/nvim-treesitter',
