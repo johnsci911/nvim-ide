b520abc45986d4f9fafa216d0c8e02533c7da80d
jkcachero <johnguitarizta@gmail.com>
2025-05-26 04:12:29 +0800

    Final choice of AI model for code completion

--- lua/config/nvim-cmp-ai.lua
+++ lua/config/nvim-cmp-ai.lua
@@ -4,19 +4,16 @@ cmp_ai:setup({
   max_lines = 100,
   provider = 'Ollama',
   provider_options = {
-    model = 'codellama:7b-code',
-    auto_unload = false, -- Set to true to automatically unload the model when
-                        -- exiting nvim.
+    model = 'qwen2.5-coder:7b-base-q6_K',
+    prompt = function(lines_before, lines_after)
+    -- You may include filetype and/or other project-wise context in this string as well.
+    -- Consult model documentation in case there are special tokens for this.
+      return "<|fim_prefix|>" .. lines_before .. "<|fim_suffix|>" .. lines_after .. "<|fim_middle|>"
+    end,
   },
   notify = true,
   notify_callback = function(msg)
     vim.notify(msg)
   end,
-  run_on_every_keystroke = true,
-  ignored_file_types = {
-    -- default is not to ignore
-    -- uncomment to ignore in lua:
-    -- lua = true
-  },
+  run_on_every_keystroke = false,
 })
-
