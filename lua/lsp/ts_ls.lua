c8df2265c94fda05944127d958ee5e8ceab8a496
jkcachero <johnkarlo.315@gmail.com>
2025-07-09 10:08:18 +0800

    Specific vue configuration

--- lua/lsp/ts_ls.lua
+++ lua/lsp/ts_ls.lua
@@ -1,20 +1,50 @@
-require'lspconfig'.ts_ls.setup{
-  init_options = {
-    plugins = {
+local lspconfig = require("lspconfig")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+local on_attach = function(client, bufnr)
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+end
+
+local npm_root = vim.fn.system("npm root -g"):gsub("%s+", "")
+local vue_ts_plugin_path = npm_root .. "/@vue/typescript-plugin"
+
+lspconfig.ts_ls.setup{
+  on_init = function(client, _)
+    client.config.init_options = client.config.init_options or {}
+    client.config.init_options.plugins = {
       {
         name = "@vue/typescript-plugin",
-        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-        languages = {
-          "javascript",
-          "typescript",
-          "vue",
-        },
+        location = vue_ts_plugin_path,
+        languages = { "javascript", "typescript", "vue" },
       },
-    },
-  },
-  filetypes = {
-    "javascript",
-    "typescript",
-    "vue",
-  },
+    }
+    client.config.init_options.typescript = {
+      preferences = {
+        importModuleSpecifierPreference = "relative",
+        includeCompletionsForModuleExports = true,
+        includeCompletionsWithInsertText = true,
+        quotePreference = "auto",
+        allowTextChangesInNewFiles = true,
+        providePrefixAndSuffixTextForRename = true,
+        allowRenameOfImportPath = true,
+        provideRefactorNotApplicableReason = true,
+        includeAutomaticOptionalChainCompletions = true,
+        includeCompletionsForImportStatements = true,
+        includeCompletionsWithSnippetText = true,
+        completeFunctionCalls = true,
+      },
+    }
+    client.config.init_options.javascript = client.config.init_options.typescript
+    client.notify("workspace/didChangeConfiguration", { settings = client.config.init_options })
+  end,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "javascript", "typescript", "vue" },
 }
+
