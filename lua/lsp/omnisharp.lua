f5659da940c3983344fcf4490aaf398ff4aa4e81
jkcachero <johnguitarizta@gmail.com>
2025-07-23 03:01:41 +0800

    Properly load sln file for omnisharp

--- lua/lsp/omnisharp.lua
+++ lua/lsp/omnisharp.lua
@@ -4,3 +4,11 @@ lspconfig.omnisharp.setup {
   cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
   root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
 }
+
+vim.api.nvim_create_augroup("sln_filetype", { clear = true })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.sln",
+  callback = function()
+    vim.bo.filetype = "cs"
+  end,
+})
