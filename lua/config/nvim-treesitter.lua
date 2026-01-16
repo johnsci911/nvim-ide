a466f8fcb7709428299ad8237bcb17f407853036
jkcachero <johnkarlo.315@gmail.com>
2026-01-17 07:35:14 +0800

    Remove nvim-treesitter parser configuration for Blade.

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -14,18 +14,7 @@ vim.api.nvim_create_autocmd('FileType', {
     end,
 })
 
--- Blade parser support
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.blade = {
-    install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-    filetype = "blade"
-}
-
--- Filetype detection
+-- Blade filetype detection
 vim.filetype.add({
     pattern = {
         ['.*%.blade%.php'] = 'blade',
